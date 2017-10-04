ed5b14bf694209e614583927e97709e996a415c1
Zondug Kim <zondugkim@zonbook-vv.lan>
2017-10-04 16:01:18 +0700

    First Commit

--- PY/TitleViewController.swift
+++ PY/TitleViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TitleViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		
+		print("ViewController is loaded")
 	}
 
 	override func didReceiveMemoryWarning() {
